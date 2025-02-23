1669dbef4d626e4486693902b12647dd84c13922
Sherwin Vishesh Jathanna <sherwin.vishesh2004@gmail.com>
2025-02-23 04:48:14 -0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import FlightSearch from "./pages/FlightSearch";
+import FlightAnalysis from "./pages/FlightAnalysis";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/flights" element={<div className="text-center text-2xl p-10">Flight Search Page (Coming Soon!)</div>} />
+        <Route path="/flights" element={<FlightSearch />} />
+        <Route path="/flightanalysis" element={<FlightAnalysis />} />
       </Routes>
     </Router>
   );
