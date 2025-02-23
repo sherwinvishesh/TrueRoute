7f3965ad38442dd04380aa78632a9a4388d3a9c8
Sherwin Vishesh Jathanna <sherwin.vishesh2004@gmail.com>
2025-02-23 07:18:36 -0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,9 @@
+// App.jsx
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import FlightSearch from "./pages/FlightSearch";
+import FlightList from "./pages/FlightList";
 import FlightAnalysis from "./pages/FlightAnalysis";
 
 function App() {
@@ -8,8 +11,10 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/flights" element={<FlightSearch />} />
+        <Route path="/search" element={<FlightSearch />} />
+        <Route path="/flights" element={<FlightList />} />
         <Route path="/flightanalysis" element={<FlightAnalysis />} />
+      
       </Routes>
     </Router>
   );
