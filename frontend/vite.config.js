e832483a38c645007a86e1eb68f004534eed0e5e
Sherwin Vishesh Jathanna <sherwin.vishesh2004@gmail.com>
2025-02-23 11:09:17 -0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,6 +11,11 @@ export default defineConfig({
   },
   server: {
     proxy: {
+      '/serpapi': {
+        target: 'https://serpapi.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/serpapi/, '')
+      },
       '/api': {
         target: process.env.VITE_API_BASE_URL || 'http://localhost:5000',
         changeOrigin: true,
